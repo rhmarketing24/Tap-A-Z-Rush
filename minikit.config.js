0528e0db16aa9fa22932cdfc438af843d64614e6
Md Raktiar Hossain <78993085+rhmarketing24@users.noreply.github.com>
2025-12-09 12:23:42 +0600

    Create minikit.config.js

---
+++ minikit.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  app: {
+    name: "Tap A → Z Rush",
+    icon: "/icon.png",
+    description: "Fastest A→Z tap challenge!",
+    website: "https://tap-a-z-rush.vercel.app"
+  }
+};
